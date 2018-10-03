9de01cabe1a75f1030e3208787c46baf270dedcc
Felix Arvidsson <felix.arvidsson@learnet.se>
2018-10-03 13:35:44 +0200

    Skapat ett bildgalleri dynamiskt med PHP.

--- htdocs/kap_5/lista_filer.php
+++ htdocs/kap_5/lista_filer.php
@@ -1,29 +1,39 @@
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lista alla filer</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-  <?php
-  /* Ange sökväg till katalogen */
-  $sokvag = "C:/xampp";
 
-    /* Skanna katalogen */
-    $filer = scandir($sokvag);
 
-    /* Skriv ut alla filer som hittades */
-    echo "<h3>Skriv ut alla filer som hittades </h3>";
-    foreach ($filer as $fil) {
-        if ($fil != "." && $fil != "..") {
-             echo "<p>$fil</p>";
-         }
-       
+
+    <?php
+/* Ange sökväg till katalogen */
+$sokvag = "./bilder";
+/* Skanna katalogen */
+$filer = scandir($sokvag);
+
+
+echo "<div class=\"kontainer\">\n";
+echo "<h1>Bildgalleri</h1>\n";
+
+foreach ($filer as $fil) {
+    if ($fil != "." && $fil != "..") {
+        echo "<div class=\"ros\">\n"; 
+        echo "<img class=\"ram vanster\" src=\"./bilder/$fil\" alt=\"lorem\">\n";
+        echo "<hr>\n";
+        echo "</div>\n";
     }
-
     
-?>  
+}
+echo "\n";
+
+?>
 </body>
+
 </html>
